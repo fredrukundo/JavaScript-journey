8ae09ebe27d6a059a6b1649322c3b5a556a2d109
Fred Rukundo <frukundo@e3r3p12.1337.ma>
2024-09-03 02:08:32 +0100

    exo04

--- JS-algorithms/4-Max-char.js
+++ JS-algorithms/4-Max-char.js
@@ -4,3 +4,27 @@
         abccccccccdd = c
         apple 12311111 = 1
 */
+function MaxChar(str)
+{
+    const charMap = {}
+    let count = 0
+    let MaxChar = ''
+    for (let char of str){
+        if (charMap[char]) {
+            charMap[char] = ++charMap[char]
+        } else {
+            charMap[char] = 1
+        }
+    }
+    //iterate through the charMap obj to find the repeated value in it
+    for (let key in charMap)
+    {
+        if (charMap[key] > count){
+            count = charMap[key]
+            MaxChar = key
+        }
+    }
+    return MaxChar
+}
+
+console.log(MaxChar("apple 23211   22341121233333"))
